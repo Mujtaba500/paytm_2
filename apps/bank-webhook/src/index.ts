48b636c1df8c55afe4275c94b80a20c76a023227
Mujtaba <126389386+Mujtaba500@users.noreply.github.com>
2025-01-06 00:26:58 +0500

    db balance update on webhook call by bank

--- apps/bank-webhook/src/index.ts
+++ apps/bank-webhook/src/index.ts
@@ -3,14 +3,60 @@ import db from "@repo/db/client";
 
 const app = express();
 
-app.post("/", (req, res) => {
+app.post("/", async (req, res) => {
   const paymentInformation = {
     token: req.body.token,
     userId: req.body.user_identifier,
     amount: req.body.amoun,
   };
 
-  // Update balance in db
+  try {
+    // Update balance in db
+    await db.balance.update({
+      where: {
+        userId: paymentInformation.userId,
+      },
+      data: {
+        amount: {
+          // Always use increment instead of amount + amount because in case of
+          // parallel requests amount will be updated wrongly
+          increment: paymentInformation.amount,
+        },
+      },
+    });
+
+    await db.onRampTransaction.update({
+      where: {
+        token: paymentInformation.token,
+      },
+      data: {
+        status: "success",
+      },
+    });
+
+    // We send the 200 response to the bank , if not the bank will refund the amount
+    // so super important
+    res.status(200).json({
+      message: "Captured",
+    });
+  } catch (err: any) {
+    console.log(
+      "Something went wrong when bank hit the express server",
+      err.message
+    );
+    await db.onRampTransaction.update({
+      where: {
+        token: paymentInformation.token,
+      },
+      data: {
+        status: "failure",
+      },
+    });
+
+    res.status(500).json({
+      message: "Internal server error",
+    });
+  }
 });
 
 // balances
